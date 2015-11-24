4f7b2ed90a20272874a0e54c6b40db037585655f
alstolbov <alstolbov@gmail.com>
2015-11-25 02:23:17 +0300

    init state

--- options/site-options.js
+++ options/site-options.js
@@ -1,9 +1,9 @@
 {
-    	"defaultContentDir": "aphorisms",
-    	"admin": {
-        	"name": "q",
-        	"pass": "w"
-    	},
+	"defaultContentDir": "home",
+	"admin": {
+		"name": "q",
+		"pass": "w"
+	},
 	"defaultHeadFile": "default/head.html",
 	"defaultBodyFile": "default/body.html",
 	"defaultFooterFile": "default/footer.html"
